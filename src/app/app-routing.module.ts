0c72e79b05fee46671bb22b97303241cc54b5924
Odmenwl <vk26vk@gmail.com>
2020-01-08 12:55:14 +0200

    lol

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthGuard} from './guards/auth.guard';
+import {GuestLayoutComponent} from './layouts/guest/guest-layout.component';
+import {MainLayoutComponent} from './layouts/main/main-layout.component';
+import {IndexComponent} from './views/index/index.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path: '',
+      component: MainLayoutComponent,
+      children: [
+        {
+          path: 'Index',
+          component: IndexComponent
+        },
+        {
+          path: '',
+          pathMatch: 'full',
+          redirectTo: 'Index'
+        }
+      ],
+      canActivate: [
+        AuthGuard
+      ]
+    },
+    {
+      path: '**',
+      redirectTo: ''
+    }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
