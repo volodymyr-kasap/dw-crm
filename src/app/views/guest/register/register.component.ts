2d7a70443836105d41a98ce7c667355c5a5d584b
odmenwl <vk26vk@gmail.com>
2020-01-27 20:29:43 +0200

    register component

---
+++ src/app/views/guest/register/register.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../../services/auth.service';
+import {AuthForm} from '../../../interfaces/auth-form';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  public registerForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  public register() {
+    if (this.registerForm.valid) {
+      const authForm: AuthForm = this.registerForm.value;
+      this.authService.makeRegister(authForm);
+    }
+  }
+
+}
