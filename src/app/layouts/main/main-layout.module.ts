99784bdca0635a83c238b46d97537780fcc2cfd2
odmenwl <vk26vk@gmail.com>
2020-02-03 01:34:19 +0200

    header #1

--- src/app/layouts/main/main-layout.module.ts
+++ src/app/layouts/main/main-layout.module.ts
@@ -5,12 +5,14 @@ import {SideMenuComponent} from './side-menu/side-menu.component';
 import {SideMenuListComponent} from './side-menu-list/side-menu-list.component';
 import {SharedModule} from '../../shared/shared.module';
 import {RouterModule} from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     MainLayoutComponent,
     SideMenuComponent,
-    SideMenuListComponent
+    SideMenuListComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
